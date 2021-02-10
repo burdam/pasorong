21dbaeb85d05dee6f16ac2110d4e0367d4176d53
burdam <antoburdam@gmail.com>
2021-02-11 04:26:46 +0900

    1

--- application/views/pegawai.php
+++ application/views/pegawai.php
@@ -24,31 +24,31 @@
     <div class="container-fluid">
         <div class="row mt-4">
             <div class="card-body">
-                <h3 class="m-0 font-weight-bold text-success d-flex justify-content-around">Data Pegawai Pengadilan Agama Sorong</h3>
+                <h4 class="m-0 font-weight-bold text-success d-flex justify-content-around">Data Pegawai Pengadilan Agama Sorong</h4>
                 <div class="table-responsive">
                     <table class="table-success table-bordered" id="dataTable" width="100%" cellspacing="0">
                         <thead>
                             <?php $no = 1; ?>
                             <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">NIP</th>
+                                <th scope="col"><center>No</center></th>
+                                <th scope="col"><center>NIP</center></th>
                                 <th scope="col"><center>Nama</center></th>
-                                <th scope="col">Golongan</th>
-                                <th scope="col">Jabatan</th>
-                                <th scope="col">Aksi</th>
-                                <th scope="col"><a href="<?= base_url('pegawai/tambah/'); ?>" class="btn btn-primary mb-2">Tambah</a></th>
+                                <th scope="col"><center>Golongan</center></th>
+                                <th scope="col"><center>Jabatan</center></th>
+                                <th scope="col"><center>Aksi</center></th>
+                                <th scope="col"><center><a href="<?= base_url('pegawai/tambah/'); ?>" class="btn btn-primary mb-2">Tambah</a></center></th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php foreach ($pegawai as $m) : ?>
                                 <tr>
-                                    <th scope="row"><?= $no++; ?></th>
+                                    <th scope="row"><center><?= $no++; ?></center></th>
                                     <td><?= $m['nip']; ?></td>
                                     <td><?= $m['nama']; ?></td>
-                                    <td><?= $m['gol']; ?></td>
+                                    <td><center><?= $m['gol']; ?></center></td>
                                     <td><?= $m['divisi']; ?></td>
-                                    <td><a href="<?= base_url('pegawai/hapus/'); ?><?= $m['id_user']; ?>" class="btn btn-danger mb-2" onclick="return confirm('Apakah Anda ingin menghapus data ini?');">Hapus</a></td>
-                                    <td><a href="<?= base_url('pegawai/ubah/'); ?><?= $m['id_user']; ?>" class="btn btn-warning mb-2">Ubah</a></td>
+                                    <td><center><a href="<?= base_url('pegawai/hapus/'); ?><?= $m['id_user']; ?>" class="btn btn-danger mb-2" onclick="return confirm('Apakah Anda ingin menghapus data ini?');">Hapus</a></center></td>
+                                    <td><center><a href="<?= base_url('pegawai/ubah/'); ?><?= $m['id_user']; ?>" class="btn btn-warning mb-2">Ubah</a></center></td>
                                 </tr>
                             <?php endforeach; ?>
                         </tbody>
